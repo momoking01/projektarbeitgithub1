3200e988ef2a24535ccb012dd09ce1ddf03b2c68
momoking01 <158822243+momoking01@users.noreply.github.com>
2024-05-26 23:26:29 +0200

    update

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import { fixupConfigRules } from "@eslint/compat";
+
+
+export default [
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...fixupConfigRules(pluginReactConfig),
+  { 
+    settings: {
+      react: {
+        version: 'detect' 
+      }
+    }
+  }
+];
