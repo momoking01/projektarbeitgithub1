ce81d93de1618dfdc616cad996d7bbba2acd7673
momoking01 <158822243+momoking01@users.noreply.github.com>
2024-05-26 22:52:27 +0200

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import playformInline from "@playform/inline";
 // https://astro.build/config
 export default defineConfig({
   integrations: [playformInline()],
-  site: 'https://momoking1.github.io',
+  site: 'https://momoking1.github.io',  //--> Name der seite:  https://momoking01.github.io/projektarbeitgithub1/
   base: '/projektarbeitgithub1/',
 });
