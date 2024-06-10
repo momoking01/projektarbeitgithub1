0f8a6d96fed454c59bdffe167486221e21cdff26
momoking01 <158822243+momoking01@users.noreply.github.com>
2024-06-10 20:26:03 +0200

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,3 @@
-// src/index.js
 
-const unusedVariable = "This is an unused variable"; // Dies ist eine nicht verwendete Variable
-const incorrectSyntax = "This line is missing a semicolon" // Fehlendes Semikolon
 
 console.log("Hello, world!");
