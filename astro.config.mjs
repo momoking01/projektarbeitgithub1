0c34a95ab5efe6484ca4e4fae5367e6386736be8
momoking01 <158822243+momoking01@users.noreply.github.com>
2024-08-11 00:12:20 +0200

    test astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,10 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://momoking01.github.io/projektarbeitgithub1',
-  base: '/projektarbeitgithub1/', // Dies ist der Name deines Repositorys
+  base: '', // Dies ist der Name deines Repositorys
   vite: {
     define: {
-      'import.meta.env.BASE_URL': JSON.stringify('/projektarbeitgithub1/'),
+      'import.meta.env.BASE_URL': JSON.stringify('/'),
     },
   },
 });
