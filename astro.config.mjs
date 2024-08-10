146ac357aae313037fa315081eaf1c52fbd95035
momoking01 <158822243+momoking01@users.noreply.github.com>
2024-08-10 23:29:43 +0200

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://momoking01.github.io',
+  base: '',
   vite: {
     define: {
       'import.meta.env.BASE_URL': JSON.stringify('/projektarbeitgithub1/'),
