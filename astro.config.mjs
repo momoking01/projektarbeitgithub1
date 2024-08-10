b0e39a23cb70822450f2e54937c5af1fc91cba14
reemalbaali <168102623+reemalbaali@users.noreply.github.com>
2024-08-11 00:09:05 +0200

    Deploy to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://momoking01.github.io/projektarbeitgithub1', 
-  base: '', 
+  site: 'https://momoking01.github.io/projektarbeitgithub1',
+  base: '/projektarbeitgithub1/', // Dies ist der Name deines Repositorys
   vite: {
     define: {
-      'import.meta.env.BASE_URL': JSON.stringify(''), 
+      'import.meta.env.BASE_URL': JSON.stringify('/projektarbeitgithub1/'),
     },
   },
 });
