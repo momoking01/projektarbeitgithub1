c203a0ee556d3b5041a8066424d32fbade2b329a
momoking01 <158822243+momoking01@users.noreply.github.com>
2024-08-11 11:28:12 +0200

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://momoking01.github.io',
+  site: 'https://momoking01.github.io/projektarbeitgithub1',
   base: '/projektarbeitgithub1/', 
   vite: {
     define: {
