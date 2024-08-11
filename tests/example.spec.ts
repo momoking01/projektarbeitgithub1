043fa7c95c71bd6e774630e77b4d9f54a678ac8c
momoking01 <158822243+momoking01@users.noreply.github.com>
2024-08-11 12:08:18 +0200

    test5

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,12 +2,12 @@ import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
   await page.goto('http://localhost:4321/projektarbeitgithub1/');
-  await expect(page).toHaveTitle(/Falscher Titel/);  
+  await expect(page).toHaveTitle(/Restaurante/);  
 });
 
 test('navigation to menu', async ({ page }) => {
   await page.goto('http://localhost:4321/projektarbeitgithub1/');
   await page.click('text=Menu');
-  await expect(page).toHaveURL('http://localhost:4321/projektarbeitgithub1/falsche-url'); 
+  await expect(page).toHaveURL('http://localhost:4321/projektarbeitgithub1/menu'); 
   await expect(page.locator('h1')).toContainText('Menu');
 });
