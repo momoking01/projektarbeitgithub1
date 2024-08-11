0dcedabbe2e05633d3d6e1c16b1932b775af036a
momoking01 <158822243+momoking01@users.noreply.github.com>
2024-08-11 11:50:13 +0200

    tset

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,10 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://momoking01.github.io/projektarbeitgithub1',
-  base: '/projektarbeitgithub1/', 
+  base: '', 
   vite: {
     define: {
-      'import.meta.env.BASE_URL': JSON.stringify('/projektarbeitgithub1/'),
+      'import.meta.env.BASE_URL': JSON.stringify('/'),
     },
   },
 });
