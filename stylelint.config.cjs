aea0c69e1ebd2b531c86abd492de59380a786219
momoking01 <158822243+momoking01@users.noreply.github.com>
2025-03-19 20:38:08 +0100

    cart.astro new

--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -3,17 +3,18 @@ module.exports = {
     "stylelint-config-standard",
     "stylelint-config-recommended",
     "stylelint-config-html",
+    "stylelint-config-astro"
   ],
   rules: {
     "block-no-empty": true,
     "color-no-invalid-hex": true,
     "selector-pseudo-class-no-unknown": true,
-    "no-descending-specificity": null,
+    "no-descending-specificity": null
   },
   overrides: [
     {
       files: ["*.astro", "**/*.astro"],
-      customSyntax: "postcss-html", // Syntax muss ein String sein
+      customSyntax: "postcss-html" // Stelle sicher, dass es ein STRING ist
     },
   ],
 };
