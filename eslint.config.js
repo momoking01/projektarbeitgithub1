b65be90dc5b7a5940dbb1f7f981c220835f63d95
momoking01 <158822243+momoking01@users.noreply.github.com>
2025-03-21 11:45:50 +0100

    1

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,11 @@
 import eslintPluginAstro from 'eslint-plugin-astro';
+
 export default [
-  // add more generic rule sets here, such as:
-  // js.confi
   ...eslintPluginAstro.configs.all,
   {
     rules: {
-      "astro/no-set-html-directive": "error"
+      "astro/no-set-html-directive": "error",
+      "astro/no-unused-css-selector": "off" // ✅ Deaktiviert die Warnung für ungenutzte CSS-Selektoren
     }
   }
 ];
